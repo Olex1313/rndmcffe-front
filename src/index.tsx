007332284a48bcfc7e888237e6b90cadf6cca634
Sergei Loshkarev <saloshkarev@miem.hse.ru>
2023-10-24 09:27:44 +0300

    landing

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./App";
 
+import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./style.css";
+
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
